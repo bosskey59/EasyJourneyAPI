d725ecac3048e504cb41de38096ed029f868c29a
Alejandro Aguilar Jr <alejandro.aguilar.jr@gmail.com>
2018-11-04 09:31:14 -0600

    fixed week

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -15,7 +15,7 @@ class FlightsController < ApplicationController
     arr_carriers.push(anytime["BrowseQuotesResponseAPIDto"]["Carriers"]["CarriersDto"])
     anytime= anytime["BrowseQuotesResponseAPIDto"]["Quotes"]["QuoteDto"]
     anytime= anytime.sort_by{|obj| obj["MinPrice"]}[0..4]
-    week, x =fetch_week
+    week, x =fetch_week(from, to)
     arr_carriers.push(x)
     arr_carriers= arr_carriers.flatten.uniq
 
@@ -23,7 +23,7 @@ class FlightsController < ApplicationController
   end
 
 
-  def fetch_week
+  def fetch_week(from, to)
     current_time = Time.now
     date_addition = 1
     day_arr=[]
@@ -32,7 +32,7 @@ class FlightsController < ApplicationController
       tmp_time = current_time + (date_addition * 86400)
       date_addition +=1
       date=tmp_time.strftime("%Y-%m-%d")
-      resp= Unirest.get("https://skyscanner-skyscanner-flight-search-v1.p.mashape.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/#{date}",  headers:{"X-Mashape-Key"=>ENV["sky_key"],"X-Mashape-Host"=>"skyscanner-skyscanner-flight-search-v1.p.mashape.com"}, parameters: nil, auth:nil)
+      resp= Unirest.get("https://skyscanner-skyscanner-flight-search-v1.p.mashape.com/apiservices/browsequotes/v1.0/US/USD/en-US/#{from}/#{to}/#{date}",  headers:{"X-Mashape-Key"=>ENV["sky_key"],"X-Mashape-Host"=>"skyscanner-skyscanner-flight-search-v1.p.mashape.com"}, parameters: nil, auth:nil)
       day=Hash.from_xml(resp.body)
       arr_carriers.push(day["BrowseQuotesResponseAPIDto"]["Carriers"]["CarriersDto"])
       day= day["BrowseQuotesResponseAPIDto"]["Quotes"]["QuoteDto"]
